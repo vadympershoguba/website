756da4342891feb5c73c3568f24ce1fde48f4cd4
Vadym Pershoguba <your-email@example.com>
2024-05-30 13:50:22 +0200

    f

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tg = window.Telegram.WebApp.onEvent();
 
  document.getElementById('sendMessageButton').addEventListener('click', () => {
+    alert(123)
     const message = prompt("Enter your message:");
     if (message !== null && message.trim() !== "") {
       fetch('http://localhost:3000/api/sendMessage', {
